8cd0c0baa64ae75f36cfb4c5105d191f8664ba32
david.vonzurmuehlen <david.vonzurmuehlen@beyondit.gmbh>
2023-09-26 11:29:29 +0200

    dm

--- chakratheme/src/components/Buttons/IconButtonPreview.jsx
+++ chakratheme/src/components/Buttons/IconButtonPreview.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Box, Text, HStack, Divider, Button } from "@chakra-ui/react";
+import { EmailIcon } from "@chakra-ui/icons";
 import { AiOutlineSearch } from "react-icons/ai";
 import MenuButtonprev from "./MenuButtonprev";
+import CommandBarButton from "./CommandBarButton";
 
 export default function IconButtonPreview() {
   return (
     <>
       <Box w={"100vw"} h={"20vh"} px={"20px"} py={"20px"}>
         <Text color={"black"} fontSize={"16px"} fontWeight={"thin"}>
-          Default IconButton
+          IconButton
         </Text>
         <Divider size={"100"} />
         <HStack pt={"20px"} spacing={"40px"}>
@@ -16,6 +18,12 @@ export default function IconButtonPreview() {
             <AiOutlineSearch />
           </Button>
           <MenuButtonprev />
+          <CommandBarButton />
+          <Button variant={"iconButton"} leftIcon={<EmailIcon />}>
+            <Text color={"black"} fontSize={14}>
+              some Text
+            </Text>
+          </Button>
         </HStack>
       </Box>
     </>
